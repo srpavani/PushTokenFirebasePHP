c1aad20b4bd5474e382e72a14501ee4469869314
diogopavani <53492119+srpavani@users.noreply.github.com>
2024-11-22 17:16:30 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,12 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-comp
 
 // Configurações do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyD_C3FeIx-ZVr37LuxhXwBsR3xe93Et4iM",
-    authDomain: "testzurich-34905.firebaseapp.com",
-    projectId: "testzurich-34905",
-    storageBucket: "testzurich-34905.firebasestorage.app",
-    messagingSenderId: "796976736672",
-    appId: "1:796976736672:web:bde687280d1d45a8f45cc5"
+    "your firebase config here"
   };
 
 // Inicializar o Firebase
